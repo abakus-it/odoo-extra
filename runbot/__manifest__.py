18a2351e2a929d555fc7dd6ee66951204fe55989
Richard Mathot <rim@odoo.com>
2016-10-25 16:09:51 +0200

    [REF] runbot: split files according to new guidelines

--- runbot/__manifest__.py
+++ runbot/__manifest__.py
@@ -11,11 +11,13 @@
         'python': ['matplotlib'],
     },
     'data': [
-        'views/runbot.xml',
-        'views/res_config_view.xml',
+        'data/runbot_data.xml',
         'security/runbot_security.xml',
         'security/ir.model.access.csv',
         'security/ir.rule.csv',
+        'views/runbot_views.xml',
+        'views/runbot_templates.xml',
+        'views/res_config_view.xml',
     ],
     'installable': True,
 }
