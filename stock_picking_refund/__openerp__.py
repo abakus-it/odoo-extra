2179283a4da18d0fb16be9668f291b2cec94ebdf
Valentin Thirion <valentinthirion@gmail.com>
2016-11-07 10:08:52 +0100

    [FIX] stock_picking_refund Corrected version number Corrected authors Corrected website

--- stock_picking_refund/__openerp__.py
+++ stock_picking_refund/__openerp__.py
@@ -3,8 +3,10 @@
 
 {
     'name': 'Decrease delivred quantity',
-    'version': '1.0',
+    'version': '9.0.1.0.1',
     'category': 'Stock',
+    'author': "Odoo SA, Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://odoo.com;http://www.abakusitsolutions.eu",
     'description': """
 This allows to decrease the quantity delivered in the
 associated SO, and therefore to generate refunds more easily.
